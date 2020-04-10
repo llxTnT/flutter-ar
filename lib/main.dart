7b123e2a934f46413b1a09c1ab4720c72b494f07
lixiaotian <lixiaotian@dobot.cc>
2020-04-10 18:20:40 +0800

    更新功能 202004101830 1.添加平面检测页面功能

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,194 @@
 import 'package:flutter/material.dart';
-import 'package:arkit_plugin/arkit_plugin.dart';
 
-void main() => runApp(MyApp());
+import 'plane_detection_page.dart';
+
+void main() => runApp(MaterialApp(home: MyApp()));
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    final samples = [
+   /*   Sample(
+        'Hello World',
+        'The simplest scene with all geometries.',
+        Icons.home,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => HelloWorldPage())),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      Sample(
+        'Check configuration',
+        'Shows which kinds of AR configuration are supported on the device',
+        Icons.settings,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => CheckSupportPage())),
+      ),
+      Sample(
+        'Earth',
+        'Sphere with an image texture and rotation animation.',
+        Icons.language,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => EarthPage())),
+      ),
+      Sample(
+        'Tap',
+        'Sphere which handles tap event.',
+        Icons.touch_app,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => TapPage())),
+      ),*/
+      Sample(
+        'Plane Detection',
+        'Detects horizontal plane.',
+        Icons.blur_on,
+            () => Navigator.of(context).push<void>(
+            MaterialPageRoute(builder: (c) => PlaneDetectionPage())),
+      )/*,
+      Sample(
+        'Distance tracking',
+        'Detects horizontal plane and track distance on it.',
+        Icons.blur_on,
+            () => Navigator.of(context).push<void>(
+            MaterialPageRoute(builder: (c) => DistanceTrackingPage())),
+      ),
+      Sample(
+        'Measure',
+        'Measures distances',
+        Icons.linear_scale,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => MeasurePage())),
+      ),
+      Sample(
+        'Physics',
+        'A sphere and a plane with dynamic and static physics',
+        Icons.file_download,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => PhysicsPage())),
+      ),
+      Sample(
+        'Image Detection',
+        'Detects Earth photo and puts a 3D object near it.',
+        Icons.collections,
+            () => Navigator.of(context).push<void>(
+            MaterialPageRoute(builder: (c) => ImageDetectionPage())),
+      ),
+      Sample(
+        'Network Image Detection',
+        'Detects Mars photo and puts a 3D object near it.',
+        Icons.collections,
+            () => Navigator.of(context).push<void>(
+            MaterialPageRoute(builder: (c) => NetworkImageDetectionPage())),
+      ),
+      Sample(
+        'Custom Light',
+        'Hello World scene with a custom light spot.',
+        Icons.lightbulb_outline,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => CustomLightPage())),
+      ),
+      Sample(
+        'Light Estimation',
+        'Estimates and applies the light around you.',
+        Icons.brightness_6,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => LightEstimatePage())),
+      ),
+      Sample(
+        'Custom Object',
+        'Place custom object on plane.',
+        Icons.nature,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => CustomObjectPage())),
+      ),
+      Sample(
+        'Occlusion',
+        'Spheres which are not visible after horizontal and vertical planes.',
+        Icons.blur_circular,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => OcclusionPage())),
+      ),
+      Sample(
+        'Manipulation',
+        'Custom objects with pinch and rotation events.',
+        Icons.threed_rotation,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => ManipulationPage())),
+      ),
+      Sample(
+        'Face Tracking',
+        'Face mask sample.',
+        Icons.face,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => FaceDetectionPage())),
+      ),
+      Sample(
+        'Panorama',
+        '360 photo sample.',
+        Icons.panorama,
+            () => Navigator.of(context)
+            .push<void>(MaterialPageRoute(builder: (c) => PanoramaPage())),
+      ),
+      Sample(
+        'Custom Animation',
+        'Custom object animation. Port of https://github.com/eh3rrera/ARKitAnimation',
+        Icons.accessibility_new,
+            () => Navigator.of(context).push<void>(
+            MaterialPageRoute(builder: (c) => CustomAnimationPage())),
+      ),
+      Sample(
+        'Widget Projection',
+        'Flutter widgets in AR',
+        Icons.widgets,
+            () => Navigator.of(context).push<void>(
+            MaterialPageRoute(builder: (c) => WidgetProjectionPage())),
+      ),
+      Sample(
+        'Real Time Updates',
+        'Calls a function once per frame',
+        Icons.timer,
+            () => Navigator.of(context).push<void>(
+            MaterialPageRoute(builder: (c) => RealTimeUpdatesPage())),
+      ),*/
+    ];
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('ARKit Demo'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body:
+      ListView(children: samples.map((s) => SampleItem(item: s)).toList()),
     );
   }
+}
+
+class SampleItem extends StatelessWidget {
+  const SampleItem({Key key, this.item}) : super(key: key);
+  final Sample item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () => item.onTap(),
+        child: ListTile(
+          leading: Icon(item.icon),
+          title: Text(
+            item.title,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          subtitle: Text(
+            item.description,
+            style: Theme.of(context).textTheme.subtitle,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Sample {
+  const Sample(this.title, this.description, this.icon, this.onTap);
+  final String title;
+  final String description;
+  final IconData icon;
+  final Function onTap;
 }
